0103da083fcec5f42009acffc4059a6938d8ccd0
JimDa <pengcheng.da@zhangmen.com>
2018-09-21 18:20:08 +0800

    练习

--- src/main/java/Practice3.java
+++ src/main/java/Practice3.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
 
 public class Practice3 {
     public static void main(String[] args) {
         Practice3 practice3 = new Practice3();
-        int i = practice3.lengthOfLongestSubstring("au");
+        int i = practice3.lengthOfLongestSubstring("bbtablud");
         System.out.println(i);
     }
 
@@ -17,20 +17,29 @@ public class Practice3 {
         }
         char[] chars = s.toCharArray();
         ArrayList<Character> uniqueChars = new ArrayList<>();
-        uniqueChars.add(chars[0]);
-        ArrayList<Integer> maxLengths = new ArrayList<>();
+        uniqueChars.add(0, chars[0]);
+        ArrayList<Integer> maxLengths = new ArrayList<>(s.length());
         int count = 1;
         for (int i = 1; i < chars.length; i++) {
             if (!uniqueChars.contains(chars[i]) && chars[i] != chars[i - 1]) {
-                count++;
-                uniqueChars.add(chars[i]);
+                uniqueChars.add(i, chars[i]);
                 maxLengths.add(count);
             } else {
+                Integer index = uniqueChars.indexOf(chars[i]);
+                for (int j = 0; j < uniqueChars.size(); j++) {
+                    if (j <= index && uniqueChars.get(j) != null) {
+                        uniqueChars.set(j, null);
+                    }
+                }
+                if (chars[i] == chars[i - 1]) {
+                    count = 1;
+                } else {
+                    count = i - uniqueChars.indexOf(chars[i]);
+                }
+                uniqueChars.add(i, chars[i]);
                 maxLengths.add(count);
-                uniqueChars.clear();
-                uniqueChars.add(chars[i]);
-                count = 1;
             }
+            count++;
         }
         return Collections.max(maxLengths);
     }
